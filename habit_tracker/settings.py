f04de5797d1d21b2caa6676d4714bda1d4e5b49b
ivanitoo <ivanvenegasvallejo@gmail.com>
2026-05-07 20:59:56 -0400

    Deploy Render

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -9,6 +9,8 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",") if os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS") else []
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -58,6 +60,11 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+import dj_database_url
+db_url = os.environ.get("DATABASE_URL")
+if db_url:
+    DATABASES["default"] = dj_database_url.parse(db_url, conn_max_age=600)
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
